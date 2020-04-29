7aa97c50963df8582762c7042e4fff9b8b4efee7
michal <michal.koziatek@protonmail.com>
2020-04-29 18:53:49 +0100

    Moved the project to michal@Yeti Formatted blog posts and fixed postgresql linking

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Blog(models.Model):
     pub_date = models.DateField()
     body = models.TextField(max_length=5000)
     images = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100] + '...'
